78a8e0e9ac85cace4c9259b7b77fb3562c3d18e1
Johnny <johnathm@usc.edu>
2016-04-28 11:28:28 -0700

    Initial Commit

---
+++ Lab6/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lab6
+//
+//  Created by Johnathan Mai on 4/28/16.
+//  Copyright © 2016 Johnathan Mai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
